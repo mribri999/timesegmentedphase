c7df8c60c8a025180ae87d2c2f3a62170dff1f44
Brian Hargreaves <bah@stanford.edu>
2026-01-06 22:04:06 -0800

    Adding example that generates nice figure showing phase extraction process.

---
+++ example2.m
@@ -0,0 +1,10 @@
+%
+%	Time Segmented Phase Extraction:  Example 2
+%
+%	Please see GitHub mribri999/timesegmentedphase.
+%
+%	This example FOLLOWS example1.m, and reproduces a figure
+%	that shows the process of the phase extraction and correction.
+
+
+% ==== A) 
